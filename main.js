0fb91069dcb1d5dcda67cc4875c1d40605ae3c41
Lê Văn Lợi <levanloi94@users.noreply.github.com>
2020-12-25 13:28:37 +0700

    fix left right

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.getElementById("catchMe").addEventListener("mouseover",mouseOver);
 function mouseOver(){
-    document.getElementById("catchMe").style.left = Math.floor(Math.random() * 500) + "px";
-    document.getElementById("catchMe").style.top = Math.floor(Math.random() * 500) + "px";
+    document.getElementById("catchMe").style.left = Math.floor(Math.random() * 90) + "rem";
+    document.getElementById("catchMe").style.top = Math.floor(Math.random() * 10) + "rem";
 }
